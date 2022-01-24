3c141484e03b47e261eecd9d0224dc82c2848bbd
raiguard <caleb.heuer@gmail.com>
2022-01-23 18:12:50 -0700

    Init players when adding to game

--- src/control.lua
+++ src/control.lua
@@ -3,16 +3,40 @@ local event = require("__flib__.event")
 
 local constants = require("constants")
 
+--- @param player_index number
+local function init_player(player_index)
+  --- @class PlayerTable
+  global.players[player_index] = {
+    flags = {
+      render_objects = {},
+      toggled = false,
+    },
+  }
+end
+
 event.on_init(function()
   global.players = {}
+
+  for player_index in pairs(game.players) do
+    init_player(player_index)
+  end
 end)
 
 event.on_player_created(function(e)
-  --- @class PlayerTable
-  global.players[e.player_index] = {}
+  init_player(e.player_index)
 end)
 
 event.register("pv-toggle", function(e)
+  local player_table = global.players[e.player_index]
+  if player_table.flags.toggled then
+    player_table.flags.toggled = false
+    for _, id in pairs(player_table.render_objects) do
+      rendering.destroy(id)
+    end
+    player_table.render_objects = {}
+    return
+  end
+  player_table.flags.toggled = true
   local player = game.get_player(e.player_index)
   local position = player.position
   local surface = player.surface
@@ -33,15 +57,16 @@ event.register("pv-toggle", function(e)
     area = tile_area,
   })
 
-  rendering.draw_rectangle({
-    left_top = tile_area.left_top,
-    right_bottom = tile_area.right_bottom,
-    filled = true,
-    color = { g = 0.2, a = 0.2 },
-    surface = surface,
-    players = { 1 },
-    time_to_live = 120,
-  })
+  -- rendering.draw_rectangle({
+  --   left_top = tile_area.left_top,
+  --   right_bottom = tile_area.right_bottom,
+  --   filled = true,
+  --   color = { g = 0.2, a = 0.2 },
+  --   surface = surface,
+  --   players = { 1 },
+  -- })
+
+  local render_objects = {}
 
   for _, entity in pairs(entities) do
     --- @type Fluid
@@ -51,14 +76,18 @@ event.register("pv-toggle", function(e)
       -- TODO: Ensure a consistent brightness and saturation
       color = game.fluid_prototypes[fluid.name].base_color
     end
-    rendering.draw_circle({
-      color = color,
-      radius = 0.3,
-      filled = true,
-      surface = surface,
-      target = entity.position,
-      players = { 1 },
-      time_to_live = 120,
-    })
+    table.insert(
+      render_objects,
+      rendering.draw_circle({
+        color = color,
+        radius = 0.3,
+        filled = true,
+        surface = surface,
+        target = entity.position,
+        players = { 1 },
+      })
+    )
+
+    player_table.render_objects = render_objects
   end
 end)
