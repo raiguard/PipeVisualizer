0d317f635e7b3451d4e45bd7bd2452e18f6cbc8a
raiguard <caleb.heuer@gmail.com>
2022-02-01 18:52:29 -0700

    Add icons

--- src/data.lua
+++ src/data.lua
@@ -14,7 +14,22 @@ data:extend({
   {
     type = "shortcut",
     name = "pv-toggle-hover",
-    icon = util.empty_sprite(),
+    icon = { filename = "__PipeVisualizer__/graphics/hover-dark-x32.png", size = 32, flags = { "gui-icon" } },
+    disabled_icon = {
+      filename = "__PipeVisualizer__/graphics/hover-dark-x32.png",
+      size = 32,
+      flags = { "gui-icon" },
+    },
+    small_icon = {
+      filename = "__PipeVisualizer__/graphics/hover-dark-x24.png",
+      size = 24,
+      flags = { "gui-icon" },
+    },
+    disabled_small_icon = {
+      filename = "__PipeVisualizer__/graphics/hover-dark-x24.png",
+      size = 24,
+      flags = { "gui-icon" },
+    },
     associated_control_input = "pv-toggle-hover",
     action = "lua",
     toggleable = true,
@@ -22,7 +37,22 @@ data:extend({
   {
     type = "shortcut",
     name = "pv-toggle-overlay",
-    icon = util.empty_sprite(),
+    icon = { filename = "__PipeVisualizer__/graphics/overlay-dark-x32.png", size = 32, flags = { "gui-icon" } },
+    disabled_icon = {
+      filename = "__PipeVisualizer__/graphics/overlay-dark-x32.png",
+      size = 32,
+      flags = { "gui-icon" },
+    },
+    small_icon = {
+      filename = "__PipeVisualizer__/graphics/overlay-dark-x24.png",
+      size = 24,
+      flags = { "gui-icon" },
+    },
+    disabled_small_icon = {
+      filename = "__PipeVisualizer__/graphics/overlay-dark-x24.png",
+      size = 24,
+      flags = { "gui-icon" },
+    },
     associated_control_input = "pv-toggle-overlay",
     action = "lua",
     toggleable = true,
