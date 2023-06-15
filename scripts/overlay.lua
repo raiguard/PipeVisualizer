584938d947e45464b7a0db0342e0ab52fd659af2
Caleb Heuer <ch@raiguard.me>
2023-06-15 17:05:02 -0600

    Resize overlay based on display resolution

--- scripts/overlay.lua
+++ scripts/overlay.lua
@@ -4,24 +4,85 @@ local flib_position = require("__flib__/position")
 -- In the source code, 200 is defined as the maximum viewable distance, but in reality it's around 220
 -- Map editor is 3x that, but we will ignore that for now
 -- Add five for a comfortable margin
-local overlay_radius = 110 + 5
-
--- --- @type table<defines.direction, MapPosition>
--- local unit_vectors = {
---   [defines.direction.north] = { x = 0, y = -1 },
---   [defines.direction.east] = { x = 1, y = 0 },
---   [defines.direction.south] = { x = 0, y = 1 },
---   [defines.direction.west] = { x = -1, y = 0 },
--- }
+local max_overlay_size = 220 + 5
 
 --- @alias RenderObjectID uint64
 --- @alias UnitNumber uint
 
 --- @class Overlay
 --- @field background RenderObjectID
+--- @field dimensions DisplayResolution
 --- @field entity_objects table<UnitNumber, RenderObjectID[]>
 --- @field last_position MapPosition
 --- @field player LuaPlayer
+
+--- @param self Overlay
+--- @param position MapPosition
+local function get_areas(self, position)
+  local dimensions = self.dimensions
+  local last_position = self.last_position
+  if not last_position then
+    return { flib_bounding_box.from_dimensions(position, dimensions.width, dimensions.height) }
+  end
+  local position_x = position.x
+  local position_y = position.y
+  local radius_x = dimensions.width / 2
+  local radius_y = dimensions.height / 2
+
+  local delta = flib_position.sub(position, last_position)
+  --- @type BoundingBox[]
+  local areas = {}
+
+  if delta.x < 0 then
+    areas[#areas + 1] = {
+      left_top = {
+        x = position_x - radius_x,
+        y = position_y - radius_y,
+      },
+      right_bottom = {
+        x = last_position.x - radius_x,
+        y = position_y + radius_y,
+      },
+    }
+  elseif delta.x > 0 then
+    areas[#areas + 1] = {
+      left_top = {
+        x = last_position.x + radius_x,
+        y = position_y - radius_y,
+      },
+      right_bottom = {
+        x = position_x + radius_x,
+        y = position_y + radius_y,
+      },
+    }
+  end
+
+  if delta.y < 0 then
+    areas[#areas + 1] = {
+      left_top = {
+        x = position_x - radius_x,
+        y = position_y - radius_y,
+      },
+      right_bottom = {
+        x = position_x + radius_x,
+        y = last_position.y - radius_y,
+      },
+    }
+  elseif delta.y > 0 then
+    areas[#areas + 1] = {
+      left_top = {
+        x = position_x - radius_x,
+        y = last_position.y + radius_y,
+      },
+      right_bottom = {
+        x = position_x + radius_x,
+        y = position_y + radius_y,
+      },
+    }
+  end
+
+  return areas
+end
 
 --- @param fluidbox LuaFluidBox
 --- @param index uint
@@ -53,6 +114,16 @@ local function get_color(fluidbox, index, colors)
   end
 
   return { r = 0.3, g = 0.3, b = 0.3 }
+end
+
+--- @param player LuaPlayer
+--- @return DisplayResolution
+local function get_dimensions(player)
+  local resolution = player.display_resolution
+  local divisor = math.max(resolution.width, resolution.height) / max_overlay_size
+  resolution.width = resolution.width / divisor
+  resolution.height = resolution.height / divisor
+  return resolution
 end
 
 --- @param self Overlay
@@ -126,69 +197,6 @@ local function visualize_entity(self, entity, colors)
 end
 
 --- @param self Overlay
---- @param position MapPosition
-local function get_areas(self, position)
-  local last_position = self.last_position
-  if not last_position then
-    return { flib_bounding_box.from_dimensions(position, overlay_radius * 2, overlay_radius * 2) }
-  end
-
-  local delta = flib_position.sub(position, last_position)
-  --- @type BoundingBox[]
-  local areas = {}
-
-  if delta.x < 0 then
-    table.insert(areas, {
-      left_top = {
-        x = position.x - overlay_radius,
-        y = position.y - overlay_radius,
-      },
-      right_bottom = {
-        x = last_position.x - overlay_radius,
-        y = position.y + overlay_radius,
-      },
-    })
-  elseif delta.x > 0 then
-    table.insert(areas, {
-      left_top = {
-        x = last_position.x + overlay_radius,
-        y = position.y - overlay_radius,
-      },
-      right_bottom = {
-        x = position.x + overlay_radius,
-        y = position.y + overlay_radius,
-      },
-    })
-  end
-
-  if delta.y < 0 then
-    table.insert(areas, {
-      left_top = {
-        x = position.x - overlay_radius,
-        y = position.y - overlay_radius,
-      },
-      right_bottom = {
-        x = position.x + overlay_radius,
-        y = last_position.y - overlay_radius,
-      },
-    })
-  elseif delta.y > 0 then
-    table.insert(areas, {
-      left_top = {
-        x = position.x - overlay_radius,
-        y = last_position.y + overlay_radius,
-      },
-      right_bottom = {
-        x = position.x + overlay_radius,
-        y = position.y + overlay_radius,
-      },
-    })
-  end
-
-  return areas
-end
-
---- @param self Overlay
 --- @param area BoundingBox
 local function visualize_area(self, area)
   local entities = self.player.surface.find_entities_filtered({
@@ -222,7 +230,7 @@ local function update_overlay(self)
   end
   local areas = get_areas(self, position)
   self.last_position = position
-  local box = flib_bounding_box.from_dimensions(position, overlay_radius * 2, overlay_radius * 2)
+  local box = flib_bounding_box.from_dimensions(position, self.dimensions.width, self.dimensions.height)
   rendering.set_left_top(self.background, box.left_top)
   rendering.set_right_bottom(self.background, box.right_bottom)
 
@@ -233,21 +241,19 @@ end
 
 --- @param player LuaPlayer
 local function create_overlay(player)
-  -- TODO: Minimize area based on current display_resolution
-  local position = flib_position.floor(player.position)
-  local box = flib_bounding_box.from_dimensions(position, overlay_radius * 2, overlay_radius * 2)
   local opacity = player.mod_settings["pv-overlay-opacity"].value --[[@as float]]
   local background = rendering.draw_rectangle({
     color = { a = opacity },
     filled = true,
-    left_top = box.left_top,
-    right_bottom = box.right_bottom,
+    left_top = { 0, 0 },
+    right_bottom = { 0, 0 },
     surface = player.surface,
     players = { player },
   })
   --- @type Overlay
   local self = {
     background = background,
+    dimensions = get_dimensions(player),
     entity_objects = {},
     player = player,
   }
@@ -292,15 +298,28 @@ local function on_player_changed_position(e)
   end
 end
 
+--- @param e EventData.on_player_display_resolution_changed
+local function on_player_display_resolution_changed(e)
+  local self = global.overlay[e.player_index]
+  if not self then
+    return
+  end
+  self.dimensions = get_dimensions(self.player)
+  self.last_position = nil
+  update_overlay(self)
+end
+
 local overlay = {}
 
 function overlay.on_init()
+  --- @type table<uint, Overlay>
   global.overlay = {}
 end
 
 overlay.events = {
   [defines.events.on_lua_shortcut] = on_toggle_overlay,
   [defines.events.on_player_changed_position] = on_player_changed_position,
+  [defines.events.on_player_display_resolution_changed] = on_player_display_resolution_changed,
   ["pv-toggle-overlay"] = on_toggle_overlay,
 }
 
