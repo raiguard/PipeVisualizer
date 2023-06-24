713f0b4520b2f00c072486261244b3a8916b6ccd
Caleb Heuer <ch@raiguard.me>
2023-06-24 16:40:45 -0600

    Make overlay less opaque by default

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ data:extend({
     type = "double-setting",
     name = "pv-overlay-opacity",
     setting_type = "runtime-per-user",
-    default_value = 0.6,
+    default_value = 0.4,
     minimum_value = 0,
     maximum_value = 1,
   },
